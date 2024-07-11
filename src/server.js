6e420a6f8a584d9a8c4768494c45353174a599a1
MarkTerrious <named1006@gmail.com>
2024-07-11 16:56:12 +0900

    some of design pattern

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
+import lookUpTable_main from "./design/condition";
+import FacotryPatternMain from "./design/factory";
 import Prototype from "./design/prototype"
+import proxy_main from "./design/proxy";
+import singleton_main from "./design/singletone";
 
 function main()
 {
-    Prototype();
+    // Prototype();
+    // singleton_main();
+    // FacotryPatternMain();
+    // proxy_main();
+    lookUpTable_main();
 }
 
 main();
